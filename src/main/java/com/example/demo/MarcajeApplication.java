4876e44506e43d75a9dedfd5d4f041b09cc18fcd
jguerra2021 <jguerrag1@gmail.com>
2021-11-10 17:55:50 -0600

    Jason

---
+++ src/main/java/com/example/demo/MarcajeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcajeApplication.class, args);
+	}
+
+}
